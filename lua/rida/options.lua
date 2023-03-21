671e5c42c86b21ea70e16c955d546717ec54591d
Rida Dzhaafar <rdzhaafar@gmail.com>
2023-03-21 10:35:51 +0200

    feat: Indent detection and whitespace rendering

--- lua/rida/options.lua
+++ lua/rida/options.lua
@@ -3,13 +3,27 @@ vim.opt.swapfile = false
 local indent_width = 4
 vim.opt.tabstop = indent_width
 vim.opt.shiftwidth = indent_width
-vim.opt.expandtab = true
+
+function set_indent_style()
+    local exp = true
+    local filetype = vim.bo.filetype
+    if filetype == "go" or filetype == "make" then
+        exp = false
+    end
+    vim.opt.expandtab = exp
+end
+
+vim.api.nvim_create_autocmd(
+    "BufReadPost",
+    { command = ":lua set_indent_style()" }
+)
+
+vim.opt.listchars.trail = "*"
+vim.opt.listchars.tab = ">"
+vim.opt.list = true
 
 vim.opt.nu = true
 vim.opt.rnu = true
 
 vim.opt.wrap = false
-
-vim.opt.listchars = { trail = "*" }
-vim.opt.list = true
 
