ab025173fbb19f62021ac4a63867eb78e21d35c6
Rida Dzhaafar <rdzhaafar@gmail.com>
2023-04-23 10:24:26 +0300

    fix: Remove faulty Go indentation detection

--- lua/rida/options.lua
+++ lua/rida/options.lua
@@ -4,20 +4,6 @@ local indent_width = 4
 vim.opt.tabstop = indent_width
 vim.opt.shiftwidth = indent_width
 
-function set_indent_style()
-    local exp = true
-    local filetype = vim.bo.filetype
-    if filetype == "go" or filetype == "make" then
-        exp = false
-    end
-    vim.opt.expandtab = exp
-end
-
-vim.api.nvim_create_autocmd(
-    "BufReadPost",
-    { command = ":lua set_indent_style()" }
-)
-
 vim.opt.listchars.trail = "*"
 vim.opt.listchars.tab = ">"
 vim.opt.list = true
