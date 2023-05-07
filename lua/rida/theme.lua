14597a9bc29b20a4d4ffd7e1285518e4510023d5
Rida Dzhaafar <rdzhaafar@gmail.com>
2023-05-07 10:24:27 +0300

    feat: Added automatic theme switching

--- lua/rida/theme.lua
+++ lua/rida/theme.lua
@@ -1,4 +1,24 @@
-vim.cmd.colorscheme("desert")
+
+-- Set colorscheme based on current system time
+function set_colorscheme()
+	local MORNING = 8
+	local EVENING = 19
+	local LIGHT_COLORSCHEME = "github_light"
+	local DARK_COLORSCHEME = "github_dark"
+
+	local hour_string = os.date("%H")
+	local hour = tonumber(hour_string)
+	if hour > MORNING and hour < EVENING then
+		vim.cmd.colorscheme(LIGHT_COLORSCHEME)
+	else
+		vim.cmd.colorscheme(DARK_COLORSCHEME)
+	end
+end
+
+local colorscheme_timer = vim.loop.new_timer()
+local MINUTE = 60 * 1000
+colorscheme_timer:start(0, MINUTE, vim.schedule_wrap(set_colorscheme))
+
 vim.opt.termguicolors = true
 
 vim.opt.guifont = "Inconsolata:h18"
