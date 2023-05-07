76f919f3724e365341a26e92ba9fe5dfa9a17559
Rida Dzhaafar <rdzhaafar@gmail.com>
2023-05-07 10:23:57 +0300

    feat: Add GitHub light/dark themes

--- lua/rida/packer.lua
+++ lua/rida/packer.lua
@@ -3,5 +3,6 @@ vim.cmd.packadd("packer.nvim")
 return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use({"projekt0n/github-nvim-theme", tag = "v0.0.7"})
 end)
 
